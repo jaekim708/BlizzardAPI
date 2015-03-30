e8b0e2454cfd373179bdd5da20c9db8cf14cc612
jaekim708 <jaekim708@gmail.com>
2015-03-30 11:27:47 -0400

    Account GET/POST working

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 __author__ = 'jae'
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, json
+from enum import Enum
 
 app = Flask(__name__)
 
 accounts = [
     {
-        'accountID': 3,
+        'accountID': 0,
         'username': 'bob',
         'faction': 'Alliance',
         'charIDs': [0],
@@ -34,11 +35,20 @@ characters = [
 def about():
     return jsonify({'author': 'Jae Il Kim', 'source': './app.py'})
 
-
-@app.route('/account', methods=['GET', 'POST'])
-def account():
+@app.route('/account', methods=['POST', 'GET'])
+def account(name=None):
+    if request.method == 'POST':
+        new_acc_id = len(accounts)
+        new_user = {
+            'accountID': new_acc_id,
+            'username': request.get_json()['username'],
+        }
+        accounts.append(new_user)
+        return jsonify({'account_id' : new_acc_id})
     if request.method == 'GET':
-        return jsonify({'accounts': accounts}), 200
+        return jsonify({'accounts': accounts})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
