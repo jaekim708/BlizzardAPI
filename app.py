3b1c1679377f1639735dfb4b113c24c646aeb501
jaekim708 <jaekim708@gmail.com>
2015-04-01 05:34:05 -0400

    Async account creation working

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, jsonify, request, abort, make_response, render_template
-from enum import Enum
+from tornado.ioloop import IOLoop
+from tornado.web import RequestHandler, Application, url, asynchronous
+from tornado.escape import json_encode, json_decode
+from tornado.concurrent import Future
+from tornado.process import fork_processes
+from tornado import gen
 
 """
 Big:
@@ -16,15 +20,14 @@ Small:
     Undeletion -
         prevent undeletion of horde chars while alliance, vice versa
         modify active status & active chars fields of char & user
-
 """
-app = Flask(__name__)
+
 
 accounts = [
     {
         'account_id': 0,
         'username': 'bob',
-        'faction': 'alliance',
+        'faction': 'Alliance',
         'char_ids': [0],
         'deleted_char_ids': [],
         'link': '{http://127.0.0.1:5000/account/bob}'
@@ -38,9 +41,9 @@ characters = [
         'char_id': 0,
         'name': 'Leeroy Jenkins',
         'level': 85,
-        'race': 'human',
-        'class': 'warrior',
-        'faction': 'alliance',
+        'race': 'Human',
+        'class': 'Warrior',
+        'faction': 'Alliance',
         'active': True
 
     }
@@ -49,34 +52,238 @@ characters = [
 FACTIONS = {'horde': ['orc', 'tauren', 'blood elf'],
             'alliance': ['human', 'gnome', 'worgen']}
 
+#
+# @app.route('/about', methods=['GET'])
+# def about():
+#     """
+#     curl http://127.0.0.1:5000/account
+#     """
+#     return jsonify({'author': 'Jae Il Kim', 'source': './app.py'})
+#
+# @app.route('/account', methods=['POST', 'GET'])
+# def account():
+#     """
+#     curl -X POST http://127.0.0.1:5000/account -d '{"username" : "Jae"}'
+#     curl -X GET http://127.0.0.1:5000/account
+#     """
+#     if request.method == 'POST':
+#         try:
+#             new_username = request.get_json(force=True)['username']
+#         except KeyError, e:
+#             raise InvalidInput('Ill-formed or misspelled request.',
+#                                status_code=404)
+#         new_acc_id = len(accounts)
+#
+#         if new_username == None or new_username == '':
+#             raise InvalidInput('Account name not specified.', status_code=400)
+#
+#         if len([acc for acc in accounts if
+#                 acc['username'] == new_username]) != 0:
+#             raise InvalidInput('Account name already taken.', status_code=400)
+#
+#         new_user = {
+#             'account_id': new_acc_id,
+#             'username': new_username,
+#             'char_ids': [],
+#             'faction': None,
+#             'link': '{http://127.0.0.1:5000/account/' + new_username + '}'
+#         }
+#         accounts.append(new_user)
+#         return jsonify({'account_id' : new_acc_id})
+#     if request.method == 'GET':
+#         return jsonify({'accounts': accounts})
+#
+# @app.route('/account/<account_name>/characters', methods=['POST'])
+# def new_char(account_name):
+#     """
+#     curl -X POST http://127.0.0.1:5000/account/Jae/characters -d '{"name": "Jar", "race": "orc", "class": "warrior", "faction": "horde", "level": 80}'
+#     """
+#     try:
+#         char_name = request.get_json(force=True)['name']
+#         char_race = request.get_json(force=True)['race'].lower()
+#         char_class = request.get_json(force=True)['class'].lower()
+#         char_faction = request.get_json(force=True)['faction'].lower()
+#         char_level = request.get_json(force=True)['level']
+#     except KeyError, e:
+#         raise InvalidInput('Ill-formed or misspelled request.',
+#                            status_code=404)
+#
+#     user_acc = [acc for acc in accounts if acc['username'] == account_name]
+#     if len(user_acc) == 0:
+#         raise InvalidInput('Specified user not found.', status_code=404)
+#     user_acc = user_acc[0]
+#
+#     if char_level < 1 or char_level > 85:
+#         raise InvalidInput('Character levels must be between 1 and 85',
+#                            status_code=400)
+#
+#     if ((char_race in FACTIONS['horde'] and char_faction == 'alliance') or
+#             (char_race in FACTIONS['alliance'] and char_faction == 'horde')):
+#         raise InvalidInput('The new character\'s race is not compatible with '
+#                            'its given faction.', status_code=400)
+#
+#     if (char_class == 'druid' and
+#             (char_race != 'tauren' and char_race != 'worgen')):
+#         raise InvalidInput('Only Tauren or Worgen can be druids.',
+#                            status_code=400)
+#     if (char_class == 'warrior' and char_race == 'blood elf'):
+#         raise InvalidInput('Blood elves cannot be warriors.', status_code=400)
+#
+#     account_faction = user_acc['faction']
+#     if account_faction is not None and account_faction != char_faction:
+#         raise InvalidInput('Account already has characters that are the '
+#                            'opposite faction of the new character.',
+#                            status_code=400)
+#
+#     new_char_id = len(characters)
+#     new_char = {
+#         'char_id': new_char_id,
+#         'name': char_name,
+#         'level': char_level,
+#         'race': char_race,
+#         'class': char_class,
+#         'faction': char_faction
+#     }
+#     characters.append(new_char)
+#     user_acc['char_ids'].append(new_char_id)
+#     user_acc['faction'] = char_faction
+#     return jsonify({'character_id': new_char_id})
+#
+#
+# @app.route('/account/<account_name>', methods=['DELETE'])
+# def delete_account(account_name):
+#     """
+#      curl -X DELETE http://127.0.0.1:5000/account/bob
+#     """
+#     user_acc = [acc for acc in accounts if acc['username'] == account_name]
+#
+#     if len(user_acc) == 0:
+#         raise InvalidInput('Specified user not found.', status_code=404)
+#     user_acc = user_acc[0]
+#
+#     for char_id in user_acc['char_ids']:
+#         del characters[char_id]
+#     del accounts[user_acc['account_id']]
+#
+#     return jsonify({'message': 'Account -' + account_name +
+#                                '- successfully deleted.'})
+#
+# @app.route('/account/<account_name>/characters/<char_name>', methods=['DELETE'])
+# def delete_character(account_name, char_name):
+#     """
+#     curl -X DELETE http://127.0.0.1:5000/account/bob/characters/Leeroy%20Jenkins
+#     """
+#     user_acc = [acc for acc in accounts if acc['username'] == account_name]
+#
+#     if len(user_acc) == 0:
+#         raise InvalidInput('Specified user not found.', status_code=404)
+#     user_acc = user_acc[0]
+#
+#     user_chars = user_acc['char_ids']
+#     character = [characters[c] for c in user_chars
+#                  if characters[c]['name'] == char_name]
+#
+#     if len(character) == 0:
+#         raise InvalidInput('Specified character not found.', status_code=404)
+#     char_to_del = character[0]
+#
+#     del user_acc['char_ids'][char_to_del['char_id']]
+#     user_acc['deleted_char_ids'].append(char_to_del['char_id'])
+#
+#     if len(user_acc['char_ids']) == 0:
+#         user_acc['faction'] = None
+#
+#     char_to_del['active'] = False
+#
+#     return jsonify({'message': 'Character -' + char_name +
+#                                '- belonging to -' + account_name +
+#                                '- was successfully deleted.'})
+#
+# @app.route('/account/<account_name>/characters', methods=['GET'])
+# def get_chars(account_name):
+#     """
+#     curl -X GET http://127.0.0.1:5000/account/bob/characters
+#
+#     """
+#     user_acc = [acc for acc in accounts if acc['username'] == account_name]
+#     if len(user_acc) == 0:
+#         raise InvalidInput('Specified user not found.', status_code=404)
+#     user_acc = user_acc[0]
+#
+#     acc_char_ids = user_acc['char_ids']
+#     acc_chars = [c for c in characters if c['char_id'] in acc_char_ids]
+#
+#     if len(user_acc['char_ids']) == 0:
+#         return jsonify({'message': 'This does not have any active characters.'})
+#     else:
+#         return jsonify({'account_id': user_acc['account_id'],
+#                         'characters': acc_chars})
+#
+#
+# class InvalidInput(Exception):
+#     """
+#     From http://flask.pocoo.org/docs/0.10/patterns/apierrors/
+#     """
+#     def __init__(self, message, status_code, payload=None):
+#         Exception.__init__(self)
+#         self.message = message
+#         self.status_code = status_code
+#         self.payload = payload
+#
+#     def to_dict(self):
+#         rv = dict(self.payload or ())
+#         rv['Error'] = self.message
+#         return rv
+#
+# @app.errorhandler(InvalidInput)
+# def handle_invalid_usage(error):
+#     response = jsonify(error.to_dict())
+#     response.status_code = error.status_code
+#     return response
 
-@app.route('/about', methods=['GET'])
-def about():
-    """
-    curl http://127.0.0.1:5000/account
-    """
-    return jsonify({'author': 'Jae Il Kim', 'source': './app.py'})
+class BaseHandler(RequestHandler):
+    def write_error(self, status_code, message=None, **kwargs):
+        if(message is not None):
+            self.write(json_encode({'Error code': status_code,
+                                    'Description': message}))
+        else:
+            self.write(json_encode({'Error code': status_code}))
+        self.set_status = status_code
+        self.finish()
 
-@app.route('/account', methods=['POST', 'GET'])
-def account():
-    """
-    curl -X POST http://127.0.0.1:5000/account -d '{"username" : "Jae"}'
-    curl -X GET http://127.0.0.1:5000/account
-    """
-    if request.method == 'POST':
+class AboutHandler(BaseHandler):
+    def get(self):
+        self.write(json_encode({'author': 'Jae Il Kim', 'source': './app.py'}))
+
+class AccountHandler(BaseHandler):
+    def get(self):
+        self.write(json_encode(accounts))
+
+    @gen.coroutine
+    def post(self):
+        response = yield self.post_account(self.request.body)
+        self.write(response)
+
+    @gen.coroutine
+    def post_account(self, input):
+        self.set_header("Content-Type", "application/json")
+        input = json_decode(self.request.body)
         try:
-            new_username = request.get_json(force=True)['username']
+            new_username = input['username']
         except KeyError, e:
-            raise InvalidInput('Ill-formed or misspelled request.',
-                               status_code=404)
+            self.write_error(404, 'Ill-formed or misspelled request.')
+            return
+
         new_acc_id = len(accounts)
 
         if new_username == None or new_username == '':
-            raise InvalidInput('Account name not specified.', status_code=400)
+            self.write_error(400, 'Account name not specified.')
+            return
 
         if len([acc for acc in accounts if
                 acc['username'] == new_username]) != 0:
-            raise InvalidInput('Account name already taken.', status_code=400)
+            self.write_error(400, 'Account name already taken.')
+            return
 
         new_user = {
             'account_id': new_acc_id,
@@ -86,159 +293,19 @@ def account():
             'link': '{http://127.0.0.1:5000/account/' + new_username + '}'
         }
         accounts.append(new_user)
-        return jsonify({'account_id' : new_acc_id})
-    if request.method == 'GET':
-        return jsonify({'accounts': accounts})
+        raise gen.Return(json_encode({'account_id' : new_acc_id}))
 
-@app.route('/account/<account_name>/characters', methods=['POST'])
-def new_char(account_name):
-    """
-    curl -X POST http://127.0.0.1:5000/account/Jae/characters -d '{"name": "Jar", "race": "orc", "class": "warrior", "faction": "horde", "level": 80}'
-    """
-    try:
-        char_name = request.get_json(force=True)['name']
-        char_race = request.get_json(force=True)['race'].lower()
-        char_class = request.get_json(force=True)['class'].lower()
-        char_faction = request.get_json(force=True)['faction'].lower()
-        char_level = request.get_json(force=True)['level']
-    except KeyError, e:
-        raise InvalidInput('Ill-formed or misspelled request.',
-                           status_code=404)
+def make_app():
+    return Application([url(r"/about", AboutHandler),
+                        url(r"/account", AccountHandler)], debug=True)
 
-    user_acc = [acc for acc in accounts if acc['username'] == account_name]
-    if len(user_acc) == 0:
-        raise InvalidInput('Specified user not found.', status_code=404)
-    user_acc = user_acc[0]
-
-    if char_level < 1 or char_level > 85:
-        raise InvalidInput('Character levels must be between 1 and 85',
-                           status_code=400)
-
-    if ((char_race in FACTIONS['horde'] and char_faction == 'alliance') or
-            (char_race in FACTIONS['alliance'] and char_faction == 'horde')):
-        raise InvalidInput('The new character\'s race is not compatible with '
-                           'its given faction.', status_code=400)
-
-    if (char_class == 'druid' and
-            (char_race != 'tauren' and char_race != 'worgen')):
-        raise InvalidInput('Only Tauren or Worgen can be druids.',
-                           status_code=400)
-    if (char_class == 'warrior' and char_race == 'blood elf'):
-        raise InvalidInput('Blood elves cannot be warriors.', status_code=400)
-
-    account_faction = user_acc['faction']
-    if account_faction is not None and account_faction != char_faction:
-        raise InvalidInput('Account already has characters that are the '
-                           'opposite faction of the new character.',
-                           status_code=400)
-
-    new_char_id = len(characters)
-    new_char = {
-        'char_id': new_char_id,
-        'name': char_name,
-        'level': char_level,
-        'race': char_race,
-        'class': char_class,
-        'faction': char_faction
-    }
-    characters.append(new_char)
-    user_acc['char_ids'].append(new_char_id)
-    user_acc['faction'] = char_faction
-    return jsonify({'character_id': new_char_id})
-
-
-@app.route('/account/<account_name>', methods=['DELETE'])
-def delete_account(account_name):
-    """
-     curl -X DELETE http://127.0.0.1:5000/account/bob
-    """
-    user_acc = [acc for acc in accounts if acc['username'] == account_name]
-
-    if len(user_acc) == 0:
-        raise InvalidInput('Specified user not found.', status_code=404)
-    user_acc = user_acc[0]
-
-    for char_id in user_acc['char_ids']:
-        del characters[char_id]
-    del accounts[user_acc['account_id']]
-
-    return jsonify({'message': 'Account -' + account_name +
-                               '- successfully deleted.'})
-
-@app.route('/account/<account_name>/characters/<char_name>', methods=['DELETE'])
-def delete_character(account_name, char_name):
-    """
-    curl -X DELETE http://127.0.0.1:5000/account/bob/characters/Leeroy%20Jenkins
-    """
-    user_acc = [acc for acc in accounts if acc['username'] == account_name]
-
-    if len(user_acc) == 0:
-        raise InvalidInput('Specified user not found.', status_code=404)
-    user_acc = user_acc[0]
-
-    user_chars = user_acc['char_ids']
-    character = [characters[c] for c in user_chars
-                 if characters[c]['name'] == char_name]
-
-    if len(character) == 0:
-        raise InvalidInput('Specified character not found.', status_code=404)
-    char_to_del = character[0]
-
-    del user_acc['char_ids'][char_to_del['char_id']]
-    user_acc['deleted_char_ids'].append(char_to_del['char_id'])
-
-    if len(user_acc['char_ids']) == 0:
-        user_acc['faction'] = None
-
-    char_to_del['active'] = False
-
-    return jsonify({'message': 'Character -' + char_name +
-                               '- belonging to -' + account_name +
-                               '- was successfully deleted.'})
-
-@app.route('/account/<account_name>/characters', methods=['GET'])
-def get_chars(account_name):
-    """
-    curl -X GET http://127.0.0.1:5000/account/bob/characters
-
-    """
-    user_acc = [acc for acc in accounts if acc['username'] == account_name]
-    if len(user_acc) == 0:
-        raise InvalidInput('Specified user not found.', status_code=404)
-    user_acc = user_acc[0]
-
-    acc_char_ids = user_acc['char_ids']
-    acc_chars = [c for c in characters if c['char_id'] in acc_char_ids]
-
-    if len(user_acc['char_ids']) == 0:
-        return jsonify({'message': 'This does not have any active characters.'})
-    else:
-        return jsonify({'account_id': user_acc['account_id'],
-                        'characters': acc_chars})
-
-
-class InvalidInput(Exception):
-    """
-    From http://flask.pocoo.org/docs/0.10/patterns/apierrors/
-    """
-    def __init__(self, message, status_code, payload=None):
-        Exception.__init__(self)
-        self.message = message
-        self.status_code = status_code
-        self.payload = payload
-
-    def to_dict(self):
-        rv = dict(self.payload or ())
-        rv['Error'] = self.message
-        return rv
-
-@app.errorhandler(InvalidInput)
-def handle_invalid_usage(error):
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
+def main():
+    app = make_app()
+    #server = tornado.httpserver.HTTPServer(app)
+    #server.bind(5000)
+    #server.start(0)
+    app.listen(5000)
+    IOLoop.current().start()
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    main()
